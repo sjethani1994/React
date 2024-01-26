ab5d89bf44f3632302ed9aa8c27b64bade9db8c9
Sumit Jethani <sjethani651@gmail.com>
2024-01-26 13:10:18 +0530

    commit with empty master branch

--- advancereact/src/Pages/HomePage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Body from '../components/Body'
-export default function HomePage({setEnablebtn,enablebtn}) {
-  return (
-    <>
-    <Body setEnablebtn={setEnablebtn} enablebtn={enablebtn}/>
-    </>
-  )
-}
